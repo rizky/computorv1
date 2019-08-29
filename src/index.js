1a42cf161b780399ed4bf7fce145c4315e21eca4
Rizky Ario <ario@riz.ky>
2019-08-29 21:44:09 +0200

    Fix printTerm

--- src/index.js
+++ src/index.js
@@ -2,51 +2,56 @@
 const _ = require('lodash');
 const print = require('terminal-kit').realTerminal ;
 
-// ((?<2>[+-])?(?:(?:(?<3>\d+)(?<4>x)|(?<5>\d+)|(?<6>x))(?:\^(?<7>\d+))?))
-const re = /((?:([+-]) +)?(?:(?:(\d+)(X)|(\d+)|(X))(?:\^(\d+))?))/g
+const inputEquations = '5 + 4X + X^2 = X^2 + 4 + 1X^1';
 
-const equations = '5 + 4X + X^2 = X^2';
-
-let match = re.exec(equations);
-const eqIndex = _.findIndex(equations, (c) => c === '=');
-
-const terms = [];
-while (match != null) {
-  const sign = (match[2] === '-' ? -1 : 1) * (eqIndex < match.index ? -1 : 1);
-  const x = match[4] || match[6];
-  const degree = match[7] ? parseInt(match[7]) : 1;
-  const coef1 = match[3] ? parseInt(match[3]) : null;
-  const coef2 = match[5] ? parseInt(match[5]) : null
-  const coef = (coef1 === null && coef2 === null) ? 1 : (coef1 || coef2)
-  terms.push({
-    coef: coef * sign,
-    degree: x ? degree : 0,
-    match: match[0],
-  });
-  match = re.exec(equations);
+const parseTerm = (equations) => {
+  // ((?<2>[+-])?(?:(?:(?<3>\d+)(?<4>x)|(?<5>\d+)|(?<6>x))(?:\^(?<7>\d+))?))
+  const re = /((?:([+-]) +)?(?:(?:(\d+)(X)|(\d+)|(X))(?:\^(\d+))?))/g
+  const terms = [];
+  let match = re.exec(equations);
+  const eqIndex = _.findIndex(_.split(equations, ''), (c) => c === '=');
+  while (match != null) {
+    const sign = (match[2] === '-' ? -1 : 1) * (eqIndex < match.index ? -1 : 1);
+    const x = match[4] || match[6];
+    const degree = match[7] ? parseInt(match[7]) : 1;
+    const coef1 = match[3] ? parseInt(match[3]) : null;
+    const coef2 = match[5] ? parseInt(match[5]) : null
+    const coef = (coef1 === null && coef2 === null) ? 1 : parseInt(coef1 || coef2)
+    terms.push({
+      coef: coef * sign,
+      degree: x ? degree : 0,
+      match: match[0],
+    });
+    match = re.exec(equations);
+  }
+  return terms;;
 }
 
-const reducedTerms = _.reduce(terms, (acc, term) => {
+const reduceTerms = (terms) => _.reduce(terms, (acc, term) => {
   const { coef: prevCoef = 0 } = acc[term.degree] || {};
   const { coef } = term;
-  const nextTerm = { ...term, coef: prevCoef + coef };
+  const nextTerm = { coef: prevCoef + coef, degree: term.degree };
+  if (nextTerm.coef === 0) return _.omit(acc, _.toString(term.degree));
   return { ...acc, [term.degree]: nextTerm };
 }, {});
 
 const printTerm = (terms) => {
-  _.map(terms, ({ coef, degree }, index) => {
-    if (index > 0 && coef !== 0) {
+  _.map(terms, ({ coef, degree }, index/* : string */) => {
+    if (parseInt(index) > 0) {
       if (coef > 0) print('+ '); else print('- ');
     }
-    if (coef > 1 && degree > 1) print('%dX%s%d ', coef, '^', degree);
-    if (coef === 1 && degree > 1) print('X%s%d ', '^', degree);
-    else if (coef > 1 && degree == 1) print('%dX ', coef);
-    else if (coef == 1 && degree == 1) print('X ', coef);
-    else if (coef > 0 && degree <= 0) print('%d ', coef);
+    const absCoef = Math.abs(coef);
+    if (absCoef > 0) print('%d', absCoef)
+    if (degree === 1) print('X ');
+    if (degree > 1) print('%s%d ', '^', degree);
+    if (degree === 0) print(' ');
   });
   print('= 0\n');
 };
 
-print('%s\n', equations);
+print('%s\n', inputEquations);
+const terms = parseTerm(inputEquations);
+const reducedTerms = reduceTerms(terms);
 printTerm(terms);
 printTerm(reducedTerms);
+console.log(reducedTerms);
