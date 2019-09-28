bbe5b634c2c940c94074b9243689f56a105a84fc
Rizky Ario <rizkyario@me.com>
2019-09-28 14:27:31 +0200

    Fix parser

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@
 const _ = require('lodash');
 const print = require('terminal-kit').realTerminal ;
 
-const inputEquations = '5 + 4X + X^2 = X^2 + 4 + 1X^1';
+const inputEquations = '8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0';
 
 const parseTerm = (equations) => {
   // ((?<2>[+-])?(?:(?:(?<3>\d+)(?<4>x)|(?<5>\d+)|(?<6>x))(?:\^(?<7>\d+))?))
-  const re = /((?:([+-]) +)?(?:(?:(\d+)(X)|(\d+)|(X))(?:\^(\d+))?))/g
+  const re = /((?:([+-])+)?(?:(?:(\d*\.?\d*)(X)|(\d+)|(X))(?:\^(\d+))?))/g
   const terms = [];
   let match = re.exec(equations);
   const eqIndex = _.findIndex(_.split(equations, ''), (c) => c === '=');
   while (match != null) {
     const sign = (match[2] === '-' ? -1 : 1) * (eqIndex < match.index ? -1 : 1);
     const x = match[4] || match[6];
-    const degree = match[7] ? parseInt(match[7]) : 1;
-    const coef1 = match[3] ? parseInt(match[3]) : null;
-    const coef2 = match[5] ? parseInt(match[5]) : null
-    const coef = (coef1 === null && coef2 === null) ? 1 : parseInt(coef1 || coef2)
+    const degree = match[7] === undefined ? 1 : parseFloat(match[7]);
+    const coef1 = match[3] === undefined ? 1 : parseFloat(match[3]);
+    const coef2 = match[5] === undefined ? 1 : parseFloat(match[5]);
+    const coef = coef1 * coef2
     terms.push({
       coef: coef * sign,
       degree: x ? degree : 0,
@@ -36,21 +36,18 @@ const reduceTerms = (terms) => _.reduce(terms, (acc, term) => {
 }, {});
 
 const printTerm = (terms) => {
-  _.map(terms, ({ coef, degree }, index/* : string */) => {
-    if (parseInt(index) > 0) {
-      if (coef > 0) print('+ '); else print('- ');
-    }
-    const absCoef = Math.abs(coef);
-    if (absCoef > 0) print('%d', absCoef)
-    if (degree === 1) print('X ');
-    if (degree > 1) print('%s%d ', '^', degree);
-    if (degree === 0) print(' ');
+  const orderedTerm = _.orderBy(terms, ['degree'], ['desc']);
+  _.map(orderedTerm, ({ coef, degree }, index/* : string */) => {
+    const powerChar = '^';
+    const sign = coef >= 0 ? '+': '-';
+    print('%s %f * X%s%d ', sign, Math.abs(coef), powerChar, degree)
   });
   print('= 0\n');
 };
 
 print('%s\n', inputEquations);
-const terms = parseTerm(inputEquations);
+const normalized = _.replace(_.replace(inputEquations, / /g, ''), /\*/g, '');
+const terms = parseTerm(normalized);
 const reducedTerms = reduceTerms(terms);
 printTerm(terms);
 printTerm(reducedTerms);
